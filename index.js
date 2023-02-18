d24bea9133c2ff7a6569281066c76fd54521d8ff
arataoka <arata.okayasu@gmail.com>
2023-02-18 22:45:25 +0900

    feat: edit index.js for more test

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ import {chromium} from "@playwright/test";
     // tab
     const browser = await chromium.launch({
         headless: false, // ブラウザ動作
-        slowMo: 500 //スローモーション
+        slowMo: 1000 //スローモーション
     });
     const page = await browser.newPage();
     await page.goto("https://artokys.netlify.app");
-    const htmlStr = await page.content();
-    console.log(htmlStr);
+    // const htmlStr = await page.content();
+    const occupationLocator =  page.locator(".sub-position");
+    const occupationText = await occupationLocator.innerText();
+
+    // get element by xpath
+    const residenceLocator = page.locator('//*[@id="About"]/div/div/div[2]/div/ul/li[3]/p');
+    const residenceText = await residenceLocator.innerText();
+    console.log(occupationText);
+    console.log(residenceText)
+    // await page.mouse.wheel(0, 3000);
+    const messageLocator = page.locator("textarea");
+    await messageLocator.type("this is me")
+    const sendButtonLocator = page.locator("text='Send'");
+    await sendButtonLocator.click();
+
     await browser.close();
 })()
