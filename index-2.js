923e8ed90c149761d1a0c5f55e0873018171e5ac
arataoka <arata.okayasu@gmail.com>
2023-02-21 20:35:20 +0900

    feat: error handling

--- index-2.js
+++ index-2.js
@@ -4,26 +4,30 @@ import {Parser} from "json2csv"
 
 (async () => {
     // tab
-    const browser = await chromium.launch({
-        headless: false, // ブラウザ動作
-        slowMo: 1000 //スローモーション
-    });
-    const page = await browser.newPage();
-    await page.goto("https://artokys.netlify.app");
-    const moreButtonLocator =  page.locator("text=More Blogs");
-    await moreButtonLocator.click();
-    const cardLocators =  page.locator(".item");
-    const cardCount = await cardLocators.count();
+    try {
+        const browser = await chromium.launch({
+            headless: false, // ブラウザ動作
+            slowMo: 1000 //スローモーション
+        });
+        const page = await browser.newPage();
+        await page.goto("https://artokys.netlify.app");
+        const moreButtonLocator =  page.locator("text=More Blogs");
+        await moreButtonLocator.click();
+        const cardLocators =  page.locator(".item");
+        const cardCount = await cardLocators.count();
 
-    const array = [];
-    for(let i=0; i< cardCount; i++){
-        const cardLocator =  cardLocators.locator(`nth=${i}`).locator(".title")
-        const text = await cardLocator.textContent();
-        array.push({name:text});
+        const array = [];
+        for(let i=0; i< cardCount; i++){
+            const cardLocator =  cardLocators.locator(`nth=${i}`).locator(".title")
+            const text = await cardLocator.textContent();
+            array.push({name:text, id:i});
+        }
+        await browser.close();
+        const parser = new Parser();
+        console.log(array);  // [{name:"text"}, {name:"text"}]
+        const csv = parser.parse(array)
+        fs.writeFileSync("./text-data.csv", csv)
+    } catch (e) {
+        console.error(e);
     }
-    await browser.close();
-    const parser = new Parser();
-    console.log(array);  // [{name:"text"}, {name:"text"}]
-    const csv = parser.parse(array)
-    fs.writeFileSync("./text-data.csv", csv)
 })()
